55792aa1087890bcf47aa0e8020b1ee54c6708db
anushh11 <anushshetty@Anushs-MacBook-Pro.local>
2019-09-06 15:12:12 -0700

    Hi

--- smart_dog_app/src/index.js
+++ smart_dog_app/src/index.js
@@ -3,50 +3,6 @@ import ReactDOM from 'react-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-<<<<<<< HEAD
-class CustomerForm extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {value: ''};
-  
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
-  
-    handleChange(event) {
-      this.setState({value: event.target.value});
-    }
-  
-    handleSubmit(event) {
-      alert('A name was submitted: ' + this.state.value);
-      event.preventDefault();
-    }
-  
-    render() {
-      return (
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            First Name:
-          </label>
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-          <input type="submit" value="Submit" />
-          <div>
-            <label>
-                Last Name:
-            </label>
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
-            <input type="submit" value="Submit" />
-          </div>
-        </form>
-      );
-    }
-  }
-
-  ReactDOM.render(
-      <CustomerForm/>,
-      document.getElementById('root')
-  )
-=======
 class CustomerForm extends React.Component  
 {
     constructor(props)
@@ -93,4 +49,3 @@ ReactDOM.render(
     <CustomerForm />,
     document.getElementById('root'),
 );
->>>>>>> 8a85ce39e905367b124c3b9a4e1adbd6bf1cc074
