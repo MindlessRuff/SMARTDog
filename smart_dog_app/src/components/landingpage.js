8e2b915d4c630789e715f529404eb0dec51bd0d3
anushh11 <anushshetty@anushs-mbp.attlocal.net>
2019-09-20 21:17:39 -0700

    uh?

---
+++ smart_dog_app/src/components/landingpage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+
+
+class Landing extends Component {
+    render(){
+        return(
+            <div style={{width: '100%', margin: 'auto'}}>
+                <grid classname= 'landing-grid'>
+                    <cell col= '12'></cell>
+                </grid>
+
+            </div>
+        )
+    }
+}
