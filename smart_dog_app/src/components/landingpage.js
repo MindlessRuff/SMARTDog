19de3ad0bcae05e5aed6eb6a08e318eaa3df5dfc
anushh11 <anushshetty@Anushs-MacBook-Pro.local>
2019-09-21 22:56:39 -0700

    Routing pages

--- smart_dog_app/src/components/landingpage.js
+++ smart_dog_app/src/components/landingpage.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
 
 
-class Landing extends Component {
+class LandingPage extends Component {
     render(){
         return(
             <div style={{width: '100%', margin: 'auto'}}>
                 <grid classname= 'landing-grid'>
                     <cell col= '12'></cell>
                 </grid>
-
+            <h1>Landing Page</h1>
             </div>
         )
     }
 }
+
+export default LandingPage;
---
+++ smart_dog_app/src/components/product.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Product extends Component {
+    render() {
+        return(
+            <div><h1>Product Page</h1></div>
+        )
+    }
+}
+
+export default Product;
