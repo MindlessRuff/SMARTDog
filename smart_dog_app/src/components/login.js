43d769e5054fad0972327105c8e75f9da56dfd4a
ksprigg@sdsu.edu <ksprigg@sdsu.edu>
2019-10-04 15:02:31 -0700

    authentication beginning

--- smart_dog_app/src/components/login.js
+++ smart_dog_app/src/components/login.js
@@ -2,12 +2,25 @@ import React, { Component } from 'react';
 import { HashRouter as Router, Route, NavLink } from 'react-router-dom';
 import CustomerLogin from '../containers/CustomerLogin';
 import SignupFormContainer from '../containers/SignupFormContainer';
+import {useAuth0} from "../auth0-wrapper";
 
 
-class Login extends Component {
-    render() {
+const Login = () => {
+        const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
         return(
             <div>
+                {!isAuthenticated && (
+                    <button
+                        onClick={() =>
+                        loginWithRedirect({})
+                    }>
+                        LOG IN
+                    </button>
+                )}
+
+                {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
+            </div>
+           /* <div>
                 <Router>
                     <div className="App">
                     <div className="App__Aside"></div>
@@ -29,10 +42,10 @@ class Login extends Component {
                     </div>
                     </div>
                 </Router>
-            </div>
+            </div>*/
             
         )
-    }
+    
 }
 
 export default Login;
