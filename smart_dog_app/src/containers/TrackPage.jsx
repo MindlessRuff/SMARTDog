a5359e3dcbbd34bc019fe213e5bcc7416500e7b9
Brandon Veltre <baveltre@gmail.com>
2019-10-25 15:37:41 -0700

    Added express package, track api, not functional.

--- smart_dog_app/src/containers/TrackPage.jsx
+++ smart_dog_app/src/containers/TrackPage.jsx
@@ -4,9 +4,32 @@ import React, { Component } from 'react';
    be with the lowest common parent of all children that use the state. If this ends up not being the lowest
    common parent, change this to a functional component (const Track = () => ) */
 class Track extends Component {
+state = {
+    data: null
+    };
+
+    componentDidMount() {
+        // Call the fetch function after the component has mounted
+        this.callBackendAPI()
+            .then(res => this.setState({ data: res.express }))
+            .catch(err => console.log(err));
+    }
+
+    // Function which will fetch the GET route from the json-server API
+    callBackendAPI = async () => {
+        const response = await fetch('/track/api');
+        const body = await response.json();
+
+        if (response.status !== 200) {
+            throw Error(body.message)
+        }
+        return body;
+    };
+
+
     render() {
         return(
-            <div><h1>Tracking Page</h1></div>
+            <div><h1>Tracking Page {this.state.data}</h1></div>
         )
     }
 }
