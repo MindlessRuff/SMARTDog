2112df3b7faf3723185874ba6c878cfac9af638a
anushh11 <anushshetty@Anushs-MBP.attlocal.net>
2019-10-24 19:19:16 -0700

    k

--- smart_dog_app/src/components/login.js
+++ smart_dog_app/src/components/login.js
@@ -1,39 +1,28 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import React from 'react';
-=======
-import React, { Component } from 'react';
->>>>>>> h
-=======
-import React from 'react';
->>>>>>> 5b7bd0991b7405d16ccc6931bb3d34288932ad40
-import {useAuth0} from "../auth0-wrapper";
-import Button from "./Button"
+import React from "react";
+import React, { Component } from "react";
+import React from "react";
+import { useAuth0 } from "../auth0-wrapper";
+import Button from "./Button";
 
 const Login = () => {
-        const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
-        if (!isAuthenticated) {
-            return (
-                <div>
-                    <Button
-                        action={loginWithRedirect}
-                        title={'LOG IN'}
-                        type={'btn btn-primary'}
-                    />
-                </div>
-            )
-        }
-        else {
-            return (
-                <div>
-                    <Button
-                        action={logout}
-                        title={'LOG OUT'}
-                        type={'btn btn-danger'}
-                    />
-                </div>    
-            )
-        }
-}
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+  if (!isAuthenticated) {
+    return (
+      <div>
+        <Button
+          action={loginWithRedirect}
+          title={"LOG IN"}
+          type={"btn btn-primary"}
+        />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Button action={logout} title={"LOG OUT"} type={"btn btn-danger"} />
+      </div>
+    );
+  }
+};
 
 export default Login;
