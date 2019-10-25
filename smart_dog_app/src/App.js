316ae79c933a01696f046b283a28ae36274ffb61
anushh11 <anushshetty@Anushs-MBP.attlocal.net>
2019-10-24 21:31:02 -0700

    raise

--- smart_dog_app/src/App.js
+++ smart_dog_app/src/App.js
@@ -1,21 +1,9 @@
 import React from "react";
-<<<<<<< HEAD
 import { useAuth0 } from "./auth0-wrapper";
 import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
 import { Link } from "react-router-dom";
-import Main from "./components/main";
-import Login from "./components/login";
-//import SignupFormContainer from "./containers/SignUpForm.js";
-require("dotenv").config();
-
-require("dotenv").config();
-=======
-import {useAuth0} from "./auth0-wrapper";
-import {Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
-import { Link } from 'react-router-dom';
-import Main from './main';
+import Main from "./main";
 import Login from "./components/Login_Out";
->>>>>>> b39242b3f668f0a01bc7323638c0afe17f29aaec
 
 function App() {
   const { loading } = useAuth0;
@@ -24,11 +12,18 @@ function App() {
     return <div>Loading...</div>;
   }
 
-<<<<<<< HEAD
   return (
     <div className="demo-big-content">
       <Layout fixedHeader>
-        <Header className="header-color" title="SMARTDog" scroll>
+        <Header
+          className="header-color"
+          title={
+            <Link to={"/"} style={{ color: "white" }}>
+              SMARTDog
+            </Link>
+          }
+          scroll
+        >
           <Navigation>
             <Link to="/about">About</Link>
             <Link to="/works">How It Works</Link>
@@ -36,7 +31,13 @@ function App() {
             <Login />
           </Navigation>
         </Header>
-        <Drawer title="SMARTDog">
+        <Drawer
+          title={
+            <Link to={"/"} style={{ color: "lightslategray" }}>
+              SMARTDog
+            </Link>
+          }
+        >
           <Navigation>
             <Link to="/about">About</Link>
             <Link to="/works">How It Works</Link>
@@ -46,38 +47,9 @@ function App() {
         </Drawer>
         <Content>
           <Main />
-          {/*{<div className='container'><SignupFormContainer /></div>}*/}
         </Content>
       </Layout>
     </div>
   );
-=======
-    return (
-        <div className="demo-big-content">
-            <Layout fixedHeader>
-                <Header className="header-color" title={<Link to={'/'} style={{color: 'white'}}>SMARTDog</Link>} scroll>
-                    <Navigation>
-                        <Link to="/about">About</Link>
-                        <Link to="/works">How It Works</Link>
-                        <Link to="/track">Track</Link>
-                        <Login/>
-                    </Navigation>
-                </Header>
-                <Drawer title = {<Link to={'/'} style={{color: 'lightslategray'}}>SMARTDog</Link>}>
-                    <Navigation>
-                        <Link to="/about">About</Link>
-                        <Link to="/works">How It Works</Link>
-                        <Link to="/track">Track</Link>
-                        <Login/>
-                    </Navigation>
-                </Drawer>
-                <Content>
-                    <Main/>
-                </Content>
-            </Layout>
-        </div>         
-    );
-    
->>>>>>> b39242b3f668f0a01bc7323638c0afe17f29aaec
 }
 export default App;
--- smart_dog_app/src/components/Button.jsx
+++ smart_dog_app/src/components/Button.jsx
@@ -2,24 +2,14 @@ import React from "react";
 import "../index.css";
 
 const Button = function(props) {
-<<<<<<< HEAD
   return (
     <button
       className={props.type} // Bootstrap 4 has built-in button styles.
       onClick={props.action}
     >
-      {props.title}
+      {props.title} {/* title = Text of the button */}
     </button>
   );
-=======
-    return(
-        <button
-            className = {props.type}    // Bootstrap 4 has built-in button styles.
-            onClick={props.action}>
-            {props.title}              {/* title = Text of the button */}
-        </button>
-    );
->>>>>>> b39242b3f668f0a01bc7323638c0afe17f29aaec
 };
 
 export default Button;
