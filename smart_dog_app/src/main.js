7d621fbba8146d2aee8639d7879f65b6cc2208e0
Kevin Sprigg <ksprigg@sdsu.edu>
2019-11-08 11:05:26 -0800

    this may be interesting

--- smart_dog_app/src/main.js
+++ smart_dog_app/src/main.js
@@ -5,16 +5,20 @@ import Works from "./containers/HowItWorksPage";
 import Track from "./containers/TrackPage";
 import PrivateRoute from "./components/PrivateRoute";
 import { Switch, Route } from "react-router-dom";
-import ProfilePage from "./containers/ProfilePage";
+import ProfilePage from "./containers/SignupFormContainer";
+import { useAuth0 } from './auth0-wrapper';
 
-const Main = () => (
-  <Switch>
-    <Route exact path="/" component={LandingPage} />
-    <Route path="/about" component={About} />
-    <Route path="/works" component={Works} />
-    <PrivateRoute path="/track" component={Track} />
-    <PrivateRoute path="/profile" component={ProfilePage} />
-  </Switch>
-);
+const Main = (props) => {
+  const { user } = useAuth0();
+  return(
+    <Switch>
+      <Route exact path="/" component={LandingPage} />
+      <Route path="/about" component={About} />
+      <Route path="/works" component={Works} />
+      <PrivateRoute path="/track" component={Track} />
+      <PrivateRoute path="/profile" render={(props) => <ProfilePage {...{user}} />} />
+    </Switch>
+  );
+};
 
 export default Main;
