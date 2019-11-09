7148d79e0b32eee3c2f6fddf97e473133c3bf9c9
anushh11 <anushshetty@Anushs-MBP.attlocal.net>
2019-11-09 11:04:01 -0800

    .

--- smart_dog_app/src/containers/SignupFormContainer.jsx
+++ smart_dog_app/src/containers/SignupFormContainer.jsx
@@ -1,62 +1,61 @@
-<<<<<<< HEAD
 import React, { Component } from "react";
 import Button from "../components/Button";
 import CustomerFormContainer from "./CustomerFormContainer";
 import axios from "axios";
 
-=======
-import React, {Component} from 'react';
-import Button from '../components/Button';
-import CustomerFormContainer from './CustomerFormContainer';
-import axios from 'axios';
-
 let port = 3000;
->>>>>>> ecf6b60aa753a03e79e252400b16c29cced7e503
 // This Signup form container is the top-level component
 // of the signup page. It holds all the state (variables)
 // and passes the variables down to its children components,
 // the individual (dog, user) containers, and from there to the textboxes
 // to keep those boxes populated with this top-level state.
 class SignupFormContainer extends Component {
-    port = 3000;
-    constructor(props) {
-        super(props);
-        this.state = {
-            userInfo: {
-                first: '',
-                last: '',
-                address: '',
-                city: '',
-                state: '',
-                zipCode: '',
-            },
-            message: ''
-        };
-        let id;     // Stores the database id on the initial page load -> get request.
-    }   
+  port = 3000;
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInfo: {
+        first: "",
+        last: "",
+        address: "",
+        city: "",
+        state: "",
+        zipCode: ""
+      },
+      message: ""
+    };
+    let id; // Stores the database id on the initial page load -> get request.
+  }
 
-    componentDidMount() {
-        let email = this.props.email;
-        // Need to use arrow functions with axios calls so that 'this' variable will
-        // refer to the class component instead of axios.
-        axios.get(`http://localhost:${port}/users?email=${email}`).then(response => {
-            let fetchedData = response.data[0].userInfo;
-            this.setState({userInfo: fetchedData, message: ''});   // [0] index since entries keyed by email are unique
-            this.id = response.data[0].id;
-            console.log(this.state);
-        })
-        .catch(error => {
-            axios.post(`http://localhost:${port}/users`,{email: this.props.email, userInfo: this.state.userInfo});
-            axios.get(`http://localhost:${port}/users?email=${email}`).then(response => {
-                let fetchedData = response.data[0].userInfo;
-                this.setState({userInfo: fetchedData, message: ''});   // [0] index since entries keyed by email are unique
-                this.id = response.data[0].id;
-            })
-            .catch(error => {
-                console.log(error);
-            });
+  componentDidMount() {
+    let email = this.props.email;
+    // Need to use arrow functions with axios calls so that 'this' variable will
+    // refer to the class component instead of axios.
+    axios
+      .get(`http://localhost:${port}/users?email=${email}`)
+      .then(response => {
+        let fetchedData = response.data[0].userInfo;
+        this.setState({ userInfo: fetchedData, message: "" }); // [0] index since entries keyed by email are unique
+        this.id = response.data[0].id;
+        console.log(this.state);
+      })
+      .catch(error => {
+        axios.post(`http://localhost:${port}/users`, {
+          email: this.props.email,
+          userInfo: this.state.userInfo
         });
-    }
+        axios
+          .get(`http://localhost:${port}/users?email=${email}`)
+          .then(response => {
+            let fetchedData = response.data[0].userInfo;
+            this.setState({ userInfo: fetchedData, message: "" }); // [0] index since entries keyed by email are unique
+            this.id = response.data[0].id;
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      });
+  }
 
   componentDidMount() {
     let email = this.props.email;
@@ -105,19 +104,23 @@ class SignupFormContainer extends Component {
     });
   };
 
-    handleFormSubmit = (event) => {
-        event.preventDefault();
-        let email = this.props.email;
-        let userInfo = this.state.userInfo;
-        this.setState({message: 'Processing...'});
-        console.log(this.state.message);
-        // Insert the id of the current user into the put request, can't do it with email key.
-        axios.put(`http://localhost:${port}/users/${this.id}`, {email: email, userInfo: userInfo})
-        .catch(error => {
-            console.log(error);
-        });
-        this.setState({message: 'Profile Updated'});
-    }
+  handleFormSubmit = event => {
+    event.preventDefault();
+    let email = this.props.email;
+    let userInfo = this.state.userInfo;
+    this.setState({ message: "Processing..." });
+    console.log(this.state.message);
+    // Insert the id of the current user into the put request, can't do it with email key.
+    axios
+      .put(`http://localhost:${port}/users/${this.id}`, {
+        email: email,
+        userInfo: userInfo
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    this.setState({ message: "Profile Updated" });
+  };
 
   handleFormClear = event => {
     event.preventDefault();
@@ -132,14 +135,15 @@ class SignupFormContainer extends Component {
     });
   };
 
-<<<<<<< HEAD
   render() {
+    console.log("SignupContainer render");
     // Destructure state into variables to pass into child components.
     // This will keep the child component textboxes populated with
     // the parent's variables. It also will change child component
     // state anytime a parent function is called, like clear or submit.
     const { first, last, address, city, state, zipCode } = this.state.userInfo;
     const userValues = { first, last, address, city, state, zipCode };
+    const { user } = this.props;
 
     return (
       <form className="container-fluid" onSubmit={this.handleFormSubmit}>
@@ -148,50 +152,18 @@ class SignupFormContainer extends Component {
             handleInputChange={this.handleInputChange}
             values={userValues}
           />
-          <div>
-            <Button
-              action={this.handleFormSubmit}
-              type={"btn btn-primary btn-block"}
-              title={"Update"}
-            />
-          </div>
+        </div>
+        <div className="container">
+          <Button
+            action={this.handleFormSubmit}
+            type={"btn btn-primary"}
+            title={"Update"}
+          />
         </div>
         <div className="result">{this.state.message}</div>
       </form>
     );
   }
-=======
-    render() {
-        console.log('SignupContainer render');
-        // Destructure state into variables to pass into child components.
-        // This will keep the child component textboxes populated with
-        // the parent's variables. It also will change child component
-        // state anytime a parent function is called, like clear or submit.
-        const {first, last, address, city, state, zipCode} = this.state.userInfo;
-        const userValues = {first, last, address, city, state, zipCode};
-        const {user} = this.props;
-        
-        return (
-            <form className='container-fluid'
-            onSubmit={this.handleFormSubmit}>
-                <div className='container'>
-                    <CustomerFormContainer 
-                        handleInputChange={this.handleInputChange}
-                        values={userValues}
-                    />
-                </div>
-                <div className='container'>
-                    <Button
-                        action={this.handleFormSubmit}
-                        type={'btn btn-primary'}
-                        title={'Update'}
-                    /> 
-                </div>
-                <div className="result">{this.state.message}</div>
-            </form>
-        )
-    }
->>>>>>> ecf6b60aa753a03e79e252400b16c29cced7e503
 }
 
 export default SignupFormContainer;
