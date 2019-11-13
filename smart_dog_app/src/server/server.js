1927142c0b8ff6e32046210f1036c04df42a54bf
Brandon Veltre <baveltre@gmail.com>
2019-11-12 17:58:18 -0800

    remove #

--- smart_dog_app/src/server/server.js
+++ smart_dog_app/src/server/server.js
@@ -5,8 +5,15 @@ const router = jsonServer.router(path.join(__dirname, "../../db.json"));
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3006;
 
+const express = require("express");
+const app = express();
+
 // Utilize Express to monitor the json server API
 server.use(middlewares);
 server.use("/", router);
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 server.listen(port, () => console.log(`Server started on port ${port}`));
 // modify
