f65e33c7b4535521e12dfdb72e87fa4a622f474b
anushh11 <anushshetty@Anushs-MBP.attlocal.net>
2019-11-13 22:09:53 -0800

    .\

--- smart_dog_app/src/server/server.js
+++ smart_dog_app/src/server/server.js
@@ -4,24 +4,17 @@ const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, "../../db.json"));
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3006;
-<<<<<<< HEAD
-=======
 
 const express = require("express");
 const app = express();
->>>>>>> f690b32b38ebb219c967d2679676857d29163210
 
 // Utilize Express to monitor the json server API
 server.use(middlewares);
 server.use("/", router);
 server.listen(port, () => console.log(`Server started on port ${port}`));
-<<<<<<< HEAD
-// modify
-=======
 // //* modify
 // console.log(path.join(__dirname, "../../build"));
 // app.use(express.static(path.join(__dirname, "build")));
 // app.get("/", function(req, res) {
 //   res.sendFile(path.join(__dirname, "../../build", "index.html"));
 // });
->>>>>>> f690b32b38ebb219c967d2679676857d29163210
