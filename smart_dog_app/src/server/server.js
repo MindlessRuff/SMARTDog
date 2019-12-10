14f7668bd0c09755f43276cb6f5e56fe767c752b
Brandon Veltre <baveltre@gmail.com>
2019-12-09 21:49:34 -0800

    static express

--- smart_dog_app/src/server/server.js
+++ smart_dog_app/src/server/server.js
@@ -11,6 +11,7 @@ const app = express();
 // Utilize Express to monitor the json server API
 server.use(middlewares);
 server.use("/api/", router);
+server.use(express.static(path.join(__dirname, "../../build")));
 server.listen(port, () => console.log(`Server started on port ${port}`));
 // //* modify
 // console.log(path.join(__dirname, "../../build"));
